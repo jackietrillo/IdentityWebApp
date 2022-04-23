647ad4e6112a1bfcdf5ab1607de5a25fee7b35be
jackie trillo <jackietrillo@hotmail.com>
2022-04-22 17:05:05 -0700

    Add project files.

---
+++ IdentityWebApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public string DefaultPolicy { get; set; }
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+             
+            DefaultPolicy = String.Empty;
+        }
+
+        public async Task<ActionResult> OnGetAsync()
+        {            
+            await Task.Run(() =>
+            {
+                DefaultPolicy = "Use this page to detail your site's privacy policy";
+            });
+
+            return Page();
+        }
+
+        public JsonResult OnGetCustomPolicy(int param, DateTime now)
+        {    
+            return new JsonResult(new { success = false, policy = "Jackie''s custom policy " + now.ToString()});
+        }
+ 
+    }
+}
